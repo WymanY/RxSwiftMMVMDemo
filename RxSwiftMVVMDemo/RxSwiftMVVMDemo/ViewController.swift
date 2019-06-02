c2335a85fd81372418319212af8f34fbfb672348
wymany <wymany@wyman.local>
2019-06-02 15:49:39 +0800

    initail demo

---
+++ RxSwiftMVVMDemo/RxSwiftMVVMDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftMVVMDemo
+//
+//  Created by wymany on 2019/6/2.
+//  Copyright © 2019 wymany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
